a0a80c1aa43002d72e8b1ddb252ecae191d1608a
jeovane96 <95777313+jeovane96@users.noreply.github.com>
2025-03-22 17:01:42 -0300

    Delete integracao directory

--- integracao/database_sistema.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-import os
-
-# db_url = os.getenv("DB_URL")
-db_url="postgresql://neondb_owner:npg_pfP1mbDI9Kry@ep-raspy-surf-a8qgy1un-pooler.eastus2.azure.neon.tech/Catagua_Streamlit?sslmode=require"
