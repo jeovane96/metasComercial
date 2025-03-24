c1104b8ac99e535855394caffacd9f95178b6364
jeovane96 <95777313+jeovane96@users.noreply.github.com>
2025-03-24 15:58:06 -0300

    Add files via upload

--- pages/cadastroMetas/listMetas.py
+++ pages/cadastroMetas/listMetas.py
@@ -8,7 +8,6 @@ def ListMetas():
     for item in listMetasCon.selecionarMetas():
         customerList.append(
             [
-                item.id, 
                 item.empreendimento,
                 item.periodo,
                 item.agrupamento_empreendimento,
@@ -21,8 +20,36 @@ def ListMetas():
 
     df = pd.DataFrame(
         customerList,
-        columns=['ID', 'Empreendimento', 'Período', 'Agrupamento Empreendimento', 'Meta', 'Considera no BI', 'Data', 'Usuário']
+        columns=['Empreendimento', 'Período', 'Agrupamento Empreendimento', 'Meta', 'Considera no BI', 'Data', 'Usuário']
     )
+
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        empreendimento        = df['Empreendimento'].unique()
+        empreendimento_filtro = st.selectbox('**Empreendimento**', options=['Todas'] + sorted(list(empreendimento)))
+
+    with col2:
+        meses = {
+            "janeiro": 1, "fevereiro": 2, "março": 3, "abril": 4, "maio": 5, "junho": 6,
+            "julho": 7, "agosto": 8, "setembro": 9, "outubro": 10, "novembro": 11, "dezembro": 12
+        }
+        def converter_periodo(periodo):
+            try:
+                mes, ano = periodo.split("/")
+                return pd.to_datetime(f"01/{meses[mes.lower()]}/{ano}", format="%d/%m/%Y")
+            except:
+                return None
+            
+        df["Período_Ordenado"]  = df["Período"].apply(converter_periodo)
+        df                      = df.sort_values(by="Período_Ordenado")
+        periodo                 = df["Período"].unique()
+        periodo_filtro          = st.selectbox("**Período**", options=["Todas"] + list(periodo))
+
+    if empreendimento_filtro != 'Todas':
+        df = df[df['Empreendimento'] == empreendimento_filtro]
+
+    if periodo_filtro != 'Todas':
+        df = df[df['Período'] == periodo_filtro]
 
     table_html = df.to_html(index=False, classes="table", border=1)
 
